996c4ec0bd33bf88994d3f93ea87b1b33acb0e6f
Richie Jr <60266955+RichieMontalvo@users.noreply.github.com>
2020-11-15 21:07:13 -0500

    Displaying Text inside the flatlist
    I am displaying text inside the flatlist

--- components/ItemList.js
+++ components/ItemList.js
@@ -1,23 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useEffect } from 'react';
+import { SafeAreaView, FlatList, StyleSheet, Text, View, Button } from 'react-native';
 
-const ItemList = ({ navigation }) => {
+const ItemList = ({ route, navigation }) => {
+
+  const { items } = route.params
+
+  useEffect( () => {
+    console.log(items)
+  })
 
   const goToDetails = () => {
     navigation.navigate('Item Details');
   }
 
+  const itemRow = ({ item }) => (
+    <View style={styles.row}>
+      <Text style={styles.title}>{item.name}</Text>
+    </View>
+  )
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        Item List
-      </Text>
-      <Button title="Go to Item Details" 
-              onPress={goToDetails}
+    <SafeAreaView style={styles.container}>
+      <FlatList 
+        data={items}
+        renderItem={itemRow}
+        keyExtractor={item => item.id}
       />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 export default ItemList;
@@ -26,11 +37,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
-    fontSize: 36,
-    marginBottom: 20,
+    fontSize: 24,
   },
+  row: {
+    padding: 20,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  }
 });
